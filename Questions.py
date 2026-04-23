bd6eec1e4d59da01077b175922699f30a61c7ed0
Tyson Orr <tmorr@calpoly.edu>
2026-04-23 09:44:52 -0700

    Commit Message

---
+++ Questions.py
@@ -0,0 +1,10 @@
+* 1. If test_double_one had been the only testing function,
+the tests would have passed the first time.
+Would that have meant that the code was correct.
+No, because the code was doing the values to the power of 2,
+rather than doubling the value
+
+* 2. While waiting to demonstrate completion of the lab,
+ponder how many tests one might need to sufficiently test
+this function.
+Atleast 5 times before it can be seen as working
